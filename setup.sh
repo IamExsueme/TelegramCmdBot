ec3a0aaf08f8c41cf48c2db9e44eab20a9071fac
Frozen12 <31291110+Frozen12@users.noreply.github.com>
2022-06-09 17:16:49 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,23 @@
-#!/bin/bash
+#! /bin/bash
+apt update && apt upgrade -y && apt install -y software-properties-common && apt update && add-apt-repository universe && add-apt-repository multiverse
+apt update && apt install -y --no-install-recommends \
+        python3.8 python3-pip curl wget git \
+        python-is-python3 build-essential \
+        nano \
+        zip unzip p7zip-full rar unrar \
+        ffmpeg \
+        rdfind
+# install node v14.x including npm
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
+apt install -y nodejs
 
-# Install rclone static binary
-wget -q https://downloads.rclone.org/v1.58.1/rclone-v1.58.1-linux-amd64.zip
-unzip -q rclone-v1.58.1-linux-amd64.zip
-export PATH=$PWD/rclone-v1.58.1-linux-amd64:$PATH
+rm -rf /var/lib/apt/lists/*
+npm install -g localtunnel yarn
+pip3 install --no-cache-dir -r requirements.txt
+
+# install aria2c
+# curl -Oaria2.deb https://tebi.com/7yhtraria2.deb
+
+
+# Install Telegram Bot app
+yarn install
