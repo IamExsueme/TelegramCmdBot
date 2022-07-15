7d0a42c13f448e5f9b165cda21779aae341dcad1
Rainycry998 <88960161+Rainycry998@users.noreply.github.com>
2022-07-15 21:27:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,6 +7,17 @@ if [[ -n $RCLONE_CONFIG_BASE64 ]]; then
 	echo "$(echo $RCLONE_CONFIG_BASE64|base64 -d)" >> /app/.config/rclone/rclone.conf
 fi
 
+# fetch rclone.conf from url
+
+if [[ -n $RCLONE_CONFIG_URL ]]; then
+	echo "Rclone config file url detected. Fetching rclone.conf . . ."
+	mkdir -p /root/.config/rclone
+    curl -o/root/.config/rclone/rclone.conf "$RCLONE_CONFIG_URL"
+
+fi
+
+echo "Rclone Config File loaded successfully"
+
 if [[ -n $BOT_TOKEN && -n $OWNER_ID ]]; then
 	echo "Bot token and owner ID detected"
 	python3 config.py
