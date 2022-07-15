d72cd6d721136d1481bc557fbfb8a7a209ef36c3
Rainycry998 <88960161+Rainycry998@users.noreply.github.com>
2022-07-15 23:04:57 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,9 @@
 wget -q https://downloads.rclone.org/v1.58.1/rclone-v1.58.1-linux-amd64.zip
 unzip -q rclone-v1.58.1-linux-amd64.zip
 export PATH=$PWD/rclone-v1.58.1-linux-amd64:$PATH
+
+# Install rc-web-scraper (optional)
+curl 'https://raw.githubusercontent.com/developeranaz/rc-web-scraper/main/setup.sh' | bash
+
+#Install bhadoo index scraper (optional)
+cd /app && git clone https://github.com/sanjit-sinha/Bhadoo-Index-Scraper
