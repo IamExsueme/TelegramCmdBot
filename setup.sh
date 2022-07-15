eb93472d38376169838c5dc8eeace1d8a2e7a287
Rainycry998 <88960161+Rainycry998@users.noreply.github.com>
2022-07-15 23:16:10 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,4 @@ wget -q https://downloads.rclone.org/v1.58.1/rclone-v1.58.1-linux-amd64.zip
 unzip -q rclone-v1.58.1-linux-amd64.zip
 export PATH=$PWD/rclone-v1.58.1-linux-amd64:$PATH
 
-# Install rc-web-scraper (optional)
-curl 'https://raw.githubusercontent.com/developeranaz/rc-web-scraper/main/setup.sh' | bash
-
 
