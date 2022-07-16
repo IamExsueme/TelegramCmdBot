0f4ca36b5db8917f5a8782de4e9d9829ff6f8031
Rainycry998 <88960161+Rainycry998@users.noreply.github.com>
2022-07-16 20:33:35 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-#Install rc-web-scraper (optional)
-curl 'https://raw.githubusercontent.com/developeranaz/rc-web-scraper/main/setup.sh' | bash 
-
-# Install rclone static binary
-wget -q https://downloads.rclone.org/v1.58.1/rclone-v1.58.1-linux-amd64.zip
-unzip -q rclone-v1.58.1-linux-amd64.zip
-export PATH=$PWD/rclone-v1.58.1-linux-amd64:$PATH
+# update code upstream
+python3 upstream.py
 
 
