a76cb8ce39290c38d0ecd352fe85c63e2bfb2399
Rainycry998 <88960161+Rainycry998@users.noreply.github.com>
2022-07-16 20:53:07 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,7 @@
 # update code upstream
 python3 upstream.py
 
+# Install rclone static binary
+wget -q https://downloads.rclone.org/v1.58.1/rclone-v1.58.1-linux-amd64.zip
+unzip -q rclone-v1.58.1-linux-amd64.zip
+export PATH=$PWD/rclone-v1.58.1-linux-amd64:$PATH
